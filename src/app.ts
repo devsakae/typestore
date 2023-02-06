f6001064b6cfb93fc87eea64f39055efe455f362
Rodrigo Sakae <devsakae@gmail.com>
2023-02-06 16:03:58 -0300

    feat: Base do CRUD com ajuda do código de Books Manager do course

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import ProductController from './controllers/product.controller';
 
 const app = express();
 
 app.use(express.json());
+
+const productController = new ProductController();
+
+app.post('/products', productController.create)
 
 export default app;
---
+++ src/services/product.service.ts
@@ -0,0 +1,18 @@
+import Product from '../interfaces/product.interface';
+import connection from '../models/connection';
+import ProductModel from '../models/product.model';
+
+class ProductService {
+  public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public create(product: Product): Promise<Product> {
+    return this.model.create(product);
+  };
+
+}
+
+export default ProductService;
